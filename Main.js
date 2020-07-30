d43c3c5758382fff8671dbc43ed8b195d1f087fe
Glitch (hello-webpage) <none>
2020-07-30 21:59:34 +0000

    🧀👚 Checkpoint ./Main.js:17665856/352

--- Main.js
+++ Main.js
@@ -21,16 +21,19 @@ function result() {
   let yItem = randomValueFromArray(insertY);
   let zItem = randomValueFromArray(insertZ);
   
-  newStory = newStory.replace(":insertx:",xItem)
-  
+  newStory = newStory.replace(":insertx:",xItem);
+  newStory = newStory.replace(":inserty:",yItem);
+  newStory = newStory.replace(":insertz:",zItem);
+
+
   if(customName.value !== '') {
     var name = customName.value;
-
+    newStory = newStory.replace("Bob",name)
   }
 
   if(document.getElementById("uk").checked) {
-    var weight = Math.round(300);
-    var temperature =  Math.round(94);
+    var weight = Math.round(300 / 14 )+ "stone";
+    var temperature =  Math.round(94 - 32) * (5/9) + "centigrade";
 
   }
 
