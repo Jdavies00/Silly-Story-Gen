d4341a0d3d294af9bc73e29d338e90be651be730
Glitch (hello-webpage) <none>
2020-07-30 21:35:46 +0000

    🌊👆 Checkpoint ./Main.js:17665856/23

--- Main.js
+++ Main.js
@@ -6,16 +6,15 @@ function randomValueFromArray(array){
   return array[Math.floor(Math.random()*array.length)];
 }
 
-var storyText  = ("It was 94 fahrenheit outside, so :insertx: went for a walk. When they got to :inserty:, they stared in horror for a few moments, then :insertz:. Bob saw the whole thing, but was not surprised — :insertx: weighs 300 pounds, and it was a hot day.")
-
+var storyText  = "It was 94 fahrenheit outside, so :insertx: went for a walk. When they got to :inserty:, they stared in horror for a few moments, then :insertz:. Bob saw the whole thing, but was not surprised — :insertx: weighs 300 pounds, and it was a hot day."
 let insertX = ["Willy the Goblin", "Big Daddy", "Father Christmas"]
 let insertY = ["the soup kitchen","Disneyland","the White House"]
 let insertZ = ["spontaneously combusted","melted into a puddle on the sidewalk","turned into a slug and crawled away"]
 
 randomize.addEventListener('click', result);
-
+// Event Listener
 function result() {
-
+  
   if(customName.value !== '') {
     var name = customName.value;
 
